cbc0f3d6e22b40fd1af0c57a993b145b60391ac5
edmundchan <pokedev8@gmail.com>
2020-05-21 23:45:21 -0700

    Updated code for presentation purposes

--- Program.cs
+++ Program.cs
@@ -11,21 +11,23 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             opentable op = new opentable();
-            op.insert(1, "123");
-            op.insert(2, "234");
-            op.insert(3, "5678");
-            op.insert(1, "875");
-            op.insert(3, "972364");
-            op.insert(4, "72963434");
-
-            op.print();
+            op.insert(1, "Size 1 Nike, White");
+            op.insert(2, "Size 2 Addidas, Black");
+            op.insert(3, "Size 3 Converse, Blue");
+            op.insert(4, "Size 4 Vans, Black");
+            op.insert(5, "Size 5 Timberlands, Brown");
+            op.insert(2, "Size 2 Addidas, White");
+            op.insert(4, "Size 4 Vans, Red");
+            op.insert(5, "Size 5 Timberlands, Black");
+            op.insert(1, "Size 1 Nike, Green");
+            op.insert(4, "Size 4 Vans, Limited Edition");
         }
 
 
 
         class opentable 
         {
-            //the linked list nodes that will be in our hash tables
+            //creating the linked list nodes that will be in our hash tables
             class hashnode
             {
                 int key;
@@ -72,29 +74,62 @@ namespace ConsoleApp1
             }
            
 
-            
-
-
             public void insert(int key, string data)
             {
                 hashnode nObj = new hashnode(key, data);
                 int hash = key % size;
+                Console.WriteLine("We are going to add shoes, {0}, into bucket {1}", data, key);
+                Console.ReadKey();
                 while (table[hash] != null && table[hash].getkey() % size != key % size)
                 {
+                    //only run this if table[hash] is not null and the the key value in the table[hash] != key%size  (if no key value, this won't run)
                     hash = (hash + 1) % size;
                 }
                 if (table[hash] != null && hash == table[hash].getkey() % size)
                 {
+                    //this runs only if table[hash] is not null and if the key at this table[hash] matches the value of the key we try to enter in
                     nObj.setNextNode(table[hash].getNextNode());
                     table[hash].setNextNode(nObj);
+                    print();
+                    Console.WriteLine();
+                    Console.Write("-----------------------------------------------------");
+                    Console.WriteLine();
                     return;
                 }
                 else
                 {
+                    //Set new hashnode into table
                     table[hash] = nObj;
+                    print();
+                    Console.WriteLine();
+                    Console.Write("-----------------------------------------------------");
+                    Console.WriteLine();
                     return;
                 }
             }
+
+            public void print()
+            {
+                hashnode current = null;
+                for (int i = 0; i < size; i++)
+                {
+                    current = table[i];
+                    int j = 0;  //int j just used for --> visuals on console
+                    while (current != null)
+                    {
+                        if (j > 0)
+                            Console.Write(" --> ");
+                        if (j == 0)
+                            Console.Write("Hash Table 'Bucket' {0} : ", i);
+                        Console.Write(current.getdata());
+                        current = current.getNextNode();
+                        j++;
+                    }
+                    Console.WriteLine();
+                }
+            }
+
+            /*
             public string retrieve(int key)
             {
                 int hash = key % size;
@@ -160,22 +195,9 @@ namespace ConsoleApp1
                     return;
                 }
             }
-            public void print()
-            {
-                hashnode current = null;
-                for (int i = 0; i < size; i++)
-                {
-                    current = table[i];
-                    while (current != null)
-                    {
-                        Console.Write(current.getdata() + " ");
-                        current = current.getNextNode();
-                    }
-                    Console.WriteLine();
-                }
-            }
 
+    */
         }
-        
+
     }
 }
